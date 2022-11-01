786b8742e0feac7fe9bebc4a1d59b81ae46553d3
= <=>
2022-11-01 13:35:15 -0400

    implemented the Contact Component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
-
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <About />
       <Skills />
       <Work />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div name='contact' className='w-full h-screen bg-black flex justify-center items-center p-4'>
+        <form action="" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-green-800 text-gray-300'>Contact</p>
+                <p className='text-gray-300 py-4'>//Submit the form below or send me an email - masarsydaniel@gmail.com</p>
+            </div>
+            <input className='p-2' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2' type="text" placeholder='Email' name='email' />
+            <textarea className='p-2' name="message" rows="10" placeholder='Message'></textarea>
+            <button className='text-white border-2 hover:bg-green-800 hover:border-green-800 px-4 py-3 my-8 mx-auto flex items-center'>Contact Me</button>
+        </form>
+    </div>
+  )
+}
+
+export default Contact
