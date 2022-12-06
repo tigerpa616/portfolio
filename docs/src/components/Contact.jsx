e4706d0e4402e559eac15fcdf4358735139d26c5
= <=>
2022-12-06 16:41:47 -0500

    fixed typo in contact form

--- docs/src/components/Contact.jsx
+++ docs/src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/b451955c-39f1-4607-9710-dbc419df328f" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-green-800 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>//Submit the form below or send me an email - masarsydaniel@gmail.com</p>
+                <p className='text-gray-300 py-4'>//Submit the form below or send me an email - masarskydaniel@gmail.com</p>
             </div>
             <input className='p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2' type="text" placeholder='Email' name='email' />
